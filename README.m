4a4cd3250f41a58cfd7531337bdaaa76dd99e6af
federicodematteis <97627039+federicodematteis@users.noreply.github.com>
2022-12-09 22:41:56 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,17 +1,17 @@
 Prima asperienza : funzioni di correlazione per l'oscillatore armonico
-  Vai in LFC/devel/montecarlo e fai girare lo script prova_azione, è il check (contiene un main, ma non è un main program) 
-  per azione.c che è la routine che serve a calcolare l'azione dell'oscillatore.
+Vai in LFC/devel/montecarlo e fai girare lo script prova_azione, è il check (contiene un main, ma non è un main program) 
+per azione.c che è la routine che serve a calcolare l'azione dell'oscillatore.
   
   Problema 1 : da dei warnings che però si possono togliere momentaneamente modificando il Makfile che si trova nella stessa cartella.
-    Ricorda che modificare il makefile non va bene, quindi bisogna trovare un'altra opzione.
+  Ricorda che modificare il makefile non va bene, quindi bisogna trovare un'altra opzione.
     
   Problema 2 : bisogna imporre periodic boundary conditions sul calcolo dell'azione: 
-    Giusti ha detto di usare l'operatore in c: i+1%N
+  Giusti ha detto di usare l'operatore in c: i+1%N
     
                                             (xx[i-1]%N-xx[i])*(xx[i-1]%N-xx[i]) 
                                             or
                                             (i-1+N)%N
- Questa espressione serve a imporre che durante il ciclo for, presente nella routine azione.c, xx[N+1] = xx[0]                                     
+Questa espressione serve a imporre che durante il ciclo for, presente nella routine azione.c, xx[N+1] = xx[0]                                     
     
 Per riassumere : nella prima esperienza abbiamo creato 3 fyle :
 in LFC/modules la routine azione.c
