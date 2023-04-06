1a140035f4c21b4698317dc95fe71a88cd06ddb1
Federico De Matteis <97627039+federicodematteis@users.noreply.github.com>
2023-04-06 12:31:37 +0200

    Add files via upload

---
+++ LFC/devel/montecarlo/first_energy_gap.c
@@ -0,0 +1,9 @@
+#define MAIN_PROGRAM
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "../../include/global.h"
+#include "../../include/sweep.h"
+#include "../../include/random.h"
+#include "../../include/azione.h"
