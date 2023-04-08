4f1b20f3f4f668026c2910c40df996327c52f47d
Federico De Matteis <97627039+federicodematteis@users.noreply.github.com>
2023-04-08 02:44:02 +0200

    Update README.m

--- README.m
+++ README.m
@@ -1,8 +1,8 @@
-Update 8 aprile
+#Update 8 aprile
 
-Federico De Matteis
-Corso di Fisica Computazionale
-Università degi studi du Milano Bicocca
-Algoritmo del metropolis per l'oscillatore armonico quantistico.
+#Federico De Matteis
+#Corso di Fisica Computazionale
+#Università degi studi du Milano Bicocca
+#Algoritmo del metropolis per l'oscillatore armonico quantistico.
 
-Cheers ^.^
+#Cheers ^.^
