5f22bb87aaca4542501b24a086c23f4f0c63f5e5
Federico De Matteis <f.dematteis2@campus.unimib.it>
2024-02-27 18:44:49 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,10 +1,10 @@
 #Update 10/5/2023
 
 #Federico De Matteis,
-#Corso di Fisica Computazionale,
-#Università degi studi di Milano Bicocca.
-#Algoritmo del metropolis per un oscillatore armonico quantistico.
-#Simulazioni oscillatore anarmonico e potenziale double well.
-#generalizzazione in 2 dimensioni dei sistemi studiati.
+#Course in Computational Physics,
+#University of Milan Bicocca.
+#Algorithm of metropolis for a quantum harmonic oscillator.
+#Simulations anharmonic oscillator and potential double well.
+#generalization in 2 dimensions of the systems studied.
 
 #Cheers ^.^
